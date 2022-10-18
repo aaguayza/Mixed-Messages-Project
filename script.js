f839fa43c1d1c467b4967177e5f7ecfa5f35d69b
ADA <adaguayza@gmail.com>
2022-10-18 12:04:50 -0500

    phrase arrays are now housed in phrases object

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-//give random passive aggressive messages
 //REMEMBER TO ADD CHANGES TO GITHUB USING PUSH ----- git push -u origin main
 
 //Imported modules
-shuffle = require('./useful-functions')
+shuffle = require('./useful-functions');
 
 
-//House 3 different arrays so that messages can trully be random
-const introCussPhrase =['Holy shit.', 'What the fuck.', 'Goddamit.', 'Jesus Fucking Christ.', 'Shit baskets.']
-const insultPhrase = ['You really don\'t know when to shut up.','At this point listining to you is like white noise.', 'You have to be the poster child of incompetentce.', 'The things that come out of your mouth never make any sense.','Your face really reminds me nature has a sense of humor.']
-const superlativePhrase = ['It\'s impressive.', 'It\'s amazing.', 'It\'s depressing.', 'It\'s irritating.', 'It\'s incredible.']
+const phrases = {
+    introCussPhrase: ['Holy shit.', 'What the fuck.', 'Goddamit.', 'Jesus Fucking Christ.', 'Shit baskets.'],
+    insultPhrase: ['You really don\'t know when to shut up.','At this point listining to you is like white noise.', 'You have to be the poster child of incompetentce.', 'The things that come out of your mouth never make any sense.','Your face really reminds me nature has a sense of humor.'],
+    superlativePhrase: ['It\'s impressive.', 'It\'s amazing.', 'It\'s depressing.', 'It\'s irritating.', 'It\'s incredible.'],
+};
+
 
 const shufflePhrases= () =>{
-    shuffle (introCussPhrase);
-    shuffle (insultPhrase);
-    shuffle (superlativePhrase);
+    shuffle (phrases.introCussPhrase);
+    shuffle (phrases.insultPhrase);
+    shuffle (phrases.superlativePhrase);
 };
 
 const makeNewPhrase = () =>{
     shufflePhrases()
-    console.log(`${introCussPhrase[0]} ${insultPhrase[0]} ${superlativePhrase[0]}`)
+    console.log(`${phrases.introCussPhrase[0]} ${phrases.insultPhrase[0]} ${phrases.superlativePhrase[0]}`)
 };
 
 makeNewPhrase();
