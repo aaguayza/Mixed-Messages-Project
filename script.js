68d89882ae678de19e0f9a38c36a64d4641dd816
ADA <adaguayza@gmail.com>
2022-10-18 10:47:15 -0500

    Random message arrays are added and filled

--- script.js
+++ script.js
@@ -5,5 +5,7 @@
 shuffle = require('./useful-functions')
 
 
-
-const messages =[]
+//House 3 different arrays so that messages can trully be random
+const introCussPhrase =['Holy shit.', 'What the fuck.', 'Goddamit.', 'Jesus Fucking Christ.', 'Shit baskets.']
+const insultPhrase = ['You really don\'t know when to shut up.','At this point listining to you is like white noise.', 'You have to be the poster child of incompetent.', 'The things that come out of your mouth never make any sense.','Your face really reminds me nature has a sense of humor.']
+const superlativePhrase = ['It\'s impressive.', 'It\'s amazing.', 'It\'s depressing.', 'It\'s irritating.', 'It\'s incredible.']
